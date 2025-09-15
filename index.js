7292b20bd37d7586453d83c156a2e58ba642aea5
deminuel <ayomidenuel21@gmail.com>
2025-09-15 05:48:41 -0700

    updated

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-const input = document.querySelector(".user");
-user.addEventListener("click", function() {
-    alert("access granted");
-} )
+const username = document.querySelector(".user");
+const pass = document.querySelector(".password");
+const btn = document.querySelector(".button");
+
+
+
+
+btn.addEventListener("click", function() {
+    if (user,passsword.test("ayo", "dasola")){
+     alert("login successful")
+}
+
+else {
+    alert("access denied")
+}}
+)
+
